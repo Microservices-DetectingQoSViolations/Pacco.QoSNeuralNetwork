778b02dc6c5429a5d43adeb141ba1225ff7b23c5
ficmichal <meho.fic@gmail.com>
2020-08-16 12:18:19 +0200

    Move scrapper outside folders.

--- src/prometheus/__init__.py
+++ src/prometheus/__init__.py
@@ -1,3 +1,2 @@
-from .scrapper import *
 from .metrics import *
 from .constants import *
--- src/scrapper.py
+++ src/scrapper.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from src.kube.constants import pod_jobs
 from src.kube.pod import get_pods
 from src.prometheus.time_utils import generate_time
-from src.prometheus.metrics import prediction_label
+from src.prometheus.metrics import metric_labels
 from src.prometheus.constants import prometheus_endpoint
 
 import tensorflow_io as tfio
@@ -14,7 +14,7 @@ data_length = 210
 
 pods_data = get_pods()
 
-dataset = tfio.experimental.IODataset.from_prometheus('aspnetcore_requests_current_total', data_length,
+dataset = tfio.experimental.IODataset.from_prometheus('rate(application_qos_violation[5s])', data_length,
                                                       offset=offset_in_ms, endpoint=prometheus_endpoint)
 
 print("Dataset Spec:\n{}\n".format(dataset.element_spec))
@@ -23,4 +23,4 @@ for (timestamp, value) in dataset:
     # time is milli second, convert to data time:
     timestamp = timestamp // 1000
     time = datetime.fromtimestamp(timestamp)
-    print("{} {}: {}".format(timestamp, time, value[pod_jobs]['10.1.39.142:80'][prediction_label]))
+    print("{} {}: {}".format(timestamp, time, value[pod_jobs]['10.1.39.142:80']['']))
