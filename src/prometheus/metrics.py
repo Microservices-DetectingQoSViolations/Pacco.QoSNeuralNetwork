b1447a9739edaaec1cc672892a9d7924be0dc5f4
ficmichal <meho.fic@gmail.com>
2020-08-17 20:36:51 +0200

    Scrape all metrics.

--- src/prometheus/metrics.py
+++ src/prometheus/metrics.py
@@ -8,42 +8,45 @@ from src.prometheus.metrics_excluder import provide_exclude, provide_exclude_cle
 exlude_route = '''route=~".+"'''
 only_without_item = '''item!~".+"'''
 
+def add_label_join(query):
+    return f'label_join({query}, "instance", "", "app")'
+
 metric_labels = {
     pod_runtime_jobs: [
-        ('aspnetcore_requests_per_second_sum', f'sum by (app) (aspnetcore_requests_per_second{provide_exclude_clear()})'),
-
-        ('aspnetcore_requests_per_second_avg', f'avg by (app) (aspnetcore_requests_per_second{provide_exclude_clear()})'),
+        ('aspnetcore_requests_per_second_sum', f'label_join(sum by (app, job) (aspnetcore_requests_per_second{provide_exclude_clear()}), "instance", "", "app")'),
 
         ('aspnetcore_requests_duration_seconds',
-         f'max by (app) (rate(aspnetcore_requests_duration_seconds_sum{{{provide_exclude() + exlude_route}}}[10s]))'),
+         f'label_join(max by (app, job) (rate(aspnetcore_requests_duration_seconds_sum{{{provide_exclude() + exlude_route}}}[10s])), "instance", "", "app")'),
 
         # 'dotnet_gc_collection_reasons_total', ? not useful
 
-        ('dotnet_gc_cpu_ratio', f'sum by (app) (rate(dotnet_gc_cpu_ratio{provide_exclude_clear()}[10s]))'),
+        # ('dotnet_gc_cpu_ratio', f'label_join(sum by (app, job) (rate(dotnet_gc_cpu_ratio{provide_exclude_clear()}[10s])), "instance", "", "app")'),
 
-        ('dotnet_gc_pause_ratio', f'sum by (app) (rate(dotnet_gc_pause_ratio{provide_exclude_clear()}[10s]))'),
+        ('dotnet_gc_pause_ratio', f'label_join(sum by (app, job) (rate(dotnet_gc_pause_ratio{provide_exclude_clear()}[10s])), "instance", "", "app")'),
 
-        ('dotnet_threadpool_adjustments_total', f'sum by (app) (rate(dotnet_threadpool_adjustments_total{provide_exclude_clear()}[10s]))'),
+        ('dotnet_threadpool_adjustments_total', f'label_join(sum by (app, job) (rate(dotnet_threadpool_adjustments_total{provide_exclude_clear()}[10s])), "instance", "", "app")'),
 
-        ('dotnet_threadpool_scheduled_total', f'sum by (app) (rate(dotnet_threadpool_scheduled_total{provide_exclude_clear()}[5s]))'),
+        ('dotnet_threadpool_scheduled_total', f'label_join(sum by (app, job) (rate(dotnet_threadpool_scheduled_total{provide_exclude_clear()}[5s])), "instance", "", "app")'),
 
-        ('runtime_lock_contention_total', f'sum by (app) (rate(runtime_lock_contention_total{provide_exclude_clear()}[10s]))'),
+        # ('runtime_lock_contention_total', f'label_join(sum by (app, job) (rate(runtime_lock_contention_total{provide_exclude_clear()}[10s])), "instance", "", "app")'),
 
-        ('runtime_threadpool_threads_total_sum', f'sum by (app) (runtime_threadpool_threads_total{provide_exclude_clear()})'),
+        ('runtime_threadpool_threads_total_sum', f'label_join(sum by (app, job) (runtime_threadpool_threads_total{provide_exclude_clear()}), "instance", "", "app")'),
 
-        ('runtime_threadpool_threads_total_min', f'min by (app) (runtime_threadpool_threads_total{provide_exclude_clear()})')
+        # ('runtime_threadpool_threads_total_min', f'label_join(min by (app, job) (runtime_threadpool_threads_total{provide_exclude_clear()}), "instance", "", "app")')
 
     ],
     pod_jobs: [
-        ('application_httprequests_errors', f'sum by (app) (application_httprequests_errors{provide_exclude_clear()})'),
+        ('application_httprequests_errors', f'label_join(sum by (app, job) (application_httprequests_errors{provide_exclude_clear()}), "instance", "", "app")'),
 
-        ('application_httprequests_error_rate_total', f'sum by (app) (rate(application_httprequests_error_rate_total{provide_exclude_clear()}[10s]))'),
+        ('application_httprequests_error_rate_total', f'label_join(sum by (app, job) (rate(application_httprequests_error_rate_total{provide_exclude_clear()}[10s])), "instance", "", "app")'),
 
-        ('application_httprequests_transactions_sum', f'sum by (app) (rate(application_httprequests_transactions_sum{provide_exclude_clear()}[10s]))'),
+        ('application_httprequests_transactions_sum', f'label_join(sum by (app, job) (rate(application_httprequests_transactions_sum{provide_exclude_clear()}[10s])), "instance", "", "app")'),
 
-        ('application_httprequests_transactions_max', f'max by (app) (rate(application_httprequests_transactions_sum{provide_exclude_clear()}[10s]))'),
+        ('application_httprequests_transactions_max', f'label_join(max by (app, job) (rate(application_httprequests_transactions_sum{provide_exclude_clear()}[10s])), "instance", "", "app")'),
 
 
-        ('application_qos_violation', f'sum by (app) (increase(application_qos_violation{{{provide_exclude() + only_without_item}}}[10s]))')
+        ('application_qos_violation', f'label_join(sum by (app, job) (increase(application_qos_violation{{{provide_exclude() + only_without_item}}}[10s])), "instance", "", "app")')
     ],
 }
+
+dummy_metric_query = "max by (job) (dotnet_build_info)"
--- src/scrapper.py
+++ src/scrapper.py
@@ -1,26 +1,51 @@
 from datetime import datetime
+from time import time
+import math
 
-from src.kube.constants import pod_jobs
-from src.kube.pod import get_pods
+from src.kube.pod import get_pod_names
 from src.prometheus.time_utils import generate_time
 from src.prometheus.metrics import metric_labels
-from src.prometheus.constants import prometheus_endpoint
+from src.prometheus.constants import prometheus_endpoint, excluding_services
 
+import tensorflow as tf
 import tensorflow_io as tfio
 
 offset = 1597501140
 offset_in_ms = offset * 1000
 data_length = 210
 
-pods_data = get_pods()
+pods = get_pod_names(excluding_services)
 
-dataset = tfio.experimental.IODataset.from_prometheus('rate(application_qos_violation[5s])', data_length,
-                                                      offset=offset_in_ms, endpoint=prometheus_endpoint)
+datasets = []
 
-print("Dataset Spec:\n{}\n".format(dataset.element_spec))
+time_series = generate_time(offset, data_length)
 
-for (timestamp, value) in dataset:
-    # time is milli second, convert to data time:
-    timestamp = timestamp // 1000
-    time = datetime.fromtimestamp(timestamp)
-    print("{} {}: {}".format(timestamp, time, value[pod_jobs]['10.1.39.142:80']['']))
+
+for job_name in metric_labels:
+    for metric in metric_labels[job_name]:
+        print(f'Download {metric} from prometheus')
+        dataset_prom = tfio.experimental.IODataset.from_prometheus(metric[1], data_length, offset=offset_in_ms, endpoint=prometheus_endpoint)
+        for pod in pods:
+            dataset = dataset_prom.map(lambda k, v: (k // 1000, v[str(job_name)][pod]['']))
+
+            dataset_list = list(dataset.as_numpy_iterator())
+            if (len(dataset_list) != len(time_series)):
+                last = dataset_list[0][1]
+                for idx, value in enumerate(time_series):
+                    try:
+                        dest_elem = dataset_list[idx]
+                    except:
+                        dataset_list.append((value, last))
+                        continue
+                    if math.isnan(dest_elem[1]):
+                        dataset_list[idx] = last
+                    if dest_elem[0] != value:
+                        dataset_list[idx:idx] = [(value, last)]
+                    last = dataset_list[idx][1]
+                dataset = tf.data.Dataset.from_tensor_slices([x[1] for x in dataset_list])
+
+            # find the max value and scale the value to [0, 1]
+            v_max = dataset.reduce(tf.constant(0.0, tf.float64), tf.math.maximum)
+            dataset = dataset.map(lambda _, v: (v / v_max))
+            datasets.append(dataset)
+print(datasets)
