50ce52994de5476cc52b80df2a8ad522284dd586
ficmichal <meho.fic@gmail.com>
2020-08-17 20:14:54 +0200

    Get distinct pod names.

--- src/kube/pod.py
+++ src/kube/pod.py
@@ -10,3 +10,15 @@ def get_pods():
 
     return [(pod.status.pod_ip + ':80', pod.metadata.labels['app'])
             for pod in pods.items if 'app' in pod.metadata.labels and 'service' in pod.metadata.labels['app']]
+
+def get_pod_names(excluding_names):
+    config.load_kube_config()
+
+    v1 = client.CoreV1Api()
+    print("Getting Pod names:")
+    pods = v1.list_pod_for_all_namespaces(watch=False)
+
+    return list(set(pod.metadata.labels['app']
+            for pod in pods.items if 'app' in pod.metadata.labels
+            and 'service' in pod.metadata.labels['app']
+            and pod.metadata.labels['app'] not in excluding_names))
