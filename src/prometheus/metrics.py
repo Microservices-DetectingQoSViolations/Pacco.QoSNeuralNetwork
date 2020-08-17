33a0dc2ff7757c9050e4aba7602001c5dea93714
ficmichal <meho.fic@gmail.com>
2020-08-17 11:28:31 +0200

    Improve metrics with aggregation functions.

--- src/prometheus/metrics.py
+++ src/prometheus/metrics.py
@@ -1,35 +1,49 @@
 from src.kube.constants import pod_jobs, pod_runtime_jobs
+from src.prometheus.metrics_excluder import provide_exclude, provide_exclude_clear
 
 # rate(x[10s]) - rate by 10 seconds range
 # sum by (instance) - sum all metric occurences by instance
 # {route=~".+"} - excluding
 
-metric_labels = {
-    pod_runtime_jobs: {
-        ('aspnetcore_requests_per_second', 'aspnetcore_requests_per_second'),  # simple pass metric
+exlude_route = '''route=~".+"'''
+only_without_item = '''item!~".+"'''
 
-        ('aspnetcore_requests_duration_seconds_sum',
-         'sum by (instance) (rate(aspnetcore_requests_duration_seconds_sum{route=~".+"}[10s]))'),
+metric_labels = {
+    pod_runtime_jobs: [
+        ('aspnetcore_requests_per_second_sum', f'sum by (app) (aspnetcore_requests_per_second{provide_exclude_clear()})'),
+
+        ('aspnetcore_requests_per_second_avg', f'avg by (app) (aspnetcore_requests_per_second{provide_exclude_clear()})'),
+
+        ('aspnetcore_requests_duration_seconds',
+         f'max by (app) (rate(aspnetcore_requests_duration_seconds_sum{{{provide_exclude() + exlude_route}}}[10s]))'),
 
         # 'dotnet_gc_collection_reasons_total', ? not useful
 
-        ('dotnet_gc_cpu_ratio', 'rate(dotnet_gc_cpu_ratio[10s])'),
+        ('dotnet_gc_cpu_ratio', f'sum by (app) (rate(dotnet_gc_cpu_ratio{provide_exclude_clear()}[10s]))'),
 
-        ('dotnet_gc_pause_ratio', 'rate(dotnet_gc_pause_ratio[10s])'),
+        ('dotnet_gc_pause_ratio', f'sum by (app) (rate(dotnet_gc_pause_ratio{provide_exclude_clear()}[10s]))'),
 
-        ('dotnet_threadpool_adjustments_total', 'rate(dotnet_threadpool_adjustments_total[10s])'),
+        ('dotnet_threadpool_adjustments_total', f'sum by (app) (rate(dotnet_threadpool_adjustments_total{provide_exclude_clear()}[10s]))'),
 
-        ('dotnet_threadpool_scheduled_total', 'rate(dotnet_threadpool_scheduled_total[5s])'),
+        ('dotnet_threadpool_scheduled_total', f'sum by (app) (rate(dotnet_threadpool_scheduled_total{provide_exclude_clear()}[5s]))'),
 
-        ('runtime_lock_contention_total', 'rate(runtime_lock_contention_total[10s])'),
+        ('runtime_lock_contention_total', f'sum by (app) (rate(runtime_lock_contention_total{provide_exclude_clear()}[10s]))'),
 
-        ('runtime_threadpool_threads_total', 'runtime_threadpool_threads_total')
-    },
-    pod_jobs: {
-        ('application_httprequests_errors', 'application_httprequests_errors'),
-        ('application_httprequests_error_rate_total', 'rate(application_httprequests_error_rate_total[10s])'),
-        ('application_httprequests_transactions_sum', 'rate(application_httprequests_transactions_sum[10s])'),
+        ('runtime_threadpool_threads_total_sum', f'sum by (app) (runtime_threadpool_threads_total{provide_exclude_clear()})'),
 
-        ('application_qos_violation', 'rate(application_qos_violation[10s])') # rate by 10 seconds range
-    },
+        ('runtime_threadpool_threads_total_min', f'min by (app) (runtime_threadpool_threads_total{provide_exclude_clear()})')
+
+    ],
+    pod_jobs: [
+        ('application_httprequests_errors', f'sum by (app) (application_httprequests_errors{provide_exclude_clear()})'),
+
+        ('application_httprequests_error_rate_total', f'sum by (app) (rate(application_httprequests_error_rate_total{provide_exclude_clear()}[10s]))'),
+
+        ('application_httprequests_transactions_sum', f'sum by (app) (rate(application_httprequests_transactions_sum{provide_exclude_clear()}[10s]))'),
+
+        ('application_httprequests_transactions_max', f'max by (app) (rate(application_httprequests_transactions_sum{provide_exclude_clear()}[10s]))'),
+
+
+        ('application_qos_violation', f'sum by (app) (increase(application_qos_violation{{{provide_exclude() + only_without_item}}}[10s]))')
+    ],
 }
--- src/prometheus/metrics_excluder.py
+++ src/prometheus/metrics_excluder.py
@@ -1,3 +1,5 @@
+from src.prometheus.constants import excluding_services
+
 def exclude_metrics_by_service_name(excluding_services, only_excluder):
     if len(excluding_services) == 0:
         return ''
@@ -8,3 +10,9 @@ def exclude_metrics_by_service_name(excluding_services, only_excluder):
         return "{" + excluding_services_query + "}"
     else:
         return excluding_services_query + ", "
+
+def provide_exclude_clear():
+    return exclude_metrics_by_service_name(excluding_services, True)
+
+def provide_exclude():
+    return exclude_metrics_by_service_name(excluding_services, False)
